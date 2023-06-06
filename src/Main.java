87f3dfc827131e47190057db35edd4009b0312f1
rajahUC <rajaho@mail.uc.edu>
2023-06-05 23:34:16 -0400

    final version

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.sql.SQLOutput;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        final int CREDIT_CARD_BALANCE = 5000;
+        final double INTEREST_RATE = 0.17;
+        double monthOneInterest = 0;
+        double monthTwoInterest = 0;
+
+        monthOneInterest = CREDIT_CARD_BALANCE * INTEREST_RATE;
+        System.out.println("The interest due after one month is " + monthOneInterest);
+
+        monthTwoInterest = (CREDIT_CARD_BALANCE + monthOneInterest) * INTEREST_RATE;
+        System.out.println("The interest due after two months is " + monthTwoInterest);
     }
 }
